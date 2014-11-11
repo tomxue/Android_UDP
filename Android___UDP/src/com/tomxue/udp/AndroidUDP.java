b34e6248e9c88b1b0aace2057217e7454acb6101
Tom Xue <tomxue0126@gmail.com>
2014-11-11 14:33:51 +0800

    add wifi enabler, but bug exists

--- Android___UDP/src/com/tomxue/udp/AndroidUDP.java
+++ Android___UDP/src/com/tomxue/udp/AndroidUDP.java
@@ -8,14 +8,15 @@ import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.Enumeration;
-
+import java.util.Timer;
+import java.util.TimerTask;
 import com.whitebyte.wifihotspotutils.ClientScanResult;
 import com.whitebyte.wifihotspotutils.FinishScanListener;
 import com.whitebyte.wifihotspotutils.WifiApManager;
-
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -41,8 +42,9 @@ public class AndroidUDP extends Activity {
 	private EditText myAddr;
 	private Button btSend, btClear, btClose;
 	private final int RECV_BUF_SZE = 4096;
-	
-	WifiApManager wifiApManager; 
+	WifiManager mWifiManager;
+
+	WifiApManager wifiApManager;
 
 	private String GetLocalIpAddress() {
 		try {
@@ -71,7 +73,7 @@ public class AndroidUDP extends Activity {
 		DatagramPacket packet = new DatagramPacket(payload.getBytes(),
 				payload.length(), ipTarget, remotePort);
 
-		if(socket != null)
+		if (socket != null && packet != null)
 			socket.send(packet);
 		// socket.disconnect();
 		// socket.close();
@@ -83,7 +85,7 @@ public class AndroidUDP extends Activity {
 		byte[] recvByteArray = new byte[RECV_BUF_SZE];
 		DatagramPacket packet = new DatagramPacket(recvByteArray,
 				recvByteArray.length);
-		if(socket != null)
+		if (socket != null)
 			socket.receive(packet);
 		else {
 			return;
@@ -95,7 +97,8 @@ public class AndroidUDP extends Activity {
 		message.what = 1;
 		Bundle bundle = new Bundle();
 		bundle.putString("recvStr", recvString);
-		bundle.putString("peerAddr", packet.getAddress().getHostAddress().toString());
+		bundle.putString("peerAddr", packet.getAddress().getHostAddress()
+				.toString());
 		bundle.putString("localAddr", GetLocalIpAddress());
 		message.setData(bundle);
 		mHandler.sendMessage(message);
@@ -148,9 +151,9 @@ public class AndroidUDP extends Activity {
 
 		socketCreate();
 		recvText.setKeyListener(null);
-		
+
 		wifiApManager = new WifiApManager(this);
-		scan();		
+		scan();
 		wifiApManager.setWifiApEnabled(null, true);
 
 		btSend.setOnClickListener(new OnClickListener() {
@@ -172,45 +175,56 @@ public class AndroidUDP extends Activity {
 				recvText.setText("");
 			}
 		});
-		
+
 		btClose.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				wifiApManager.setWifiApEnabled(null, false);
-				
-				// open wifi settings GUI for users, user has choice for the next action: to enable wifi or not
-				final Intent intent = new Intent(Intent.ACTION_MAIN, null);
-		        intent.addCategory(Intent.CATEGORY_LAUNCHER);
-		        final ComponentName cn = new ComponentName("com.android.settings", "com.android.settings.wifi.WifiSettings");
-		        intent.setComponent(cn);
-		        intent.setFlags(intent.FLAG_ACTIVITY_NEW_TASK);
-		        startActivity(intent);
-		        
+
+				// open wifi settings GUI for users, user has choice for the
+				// next action: to enable wifi or not
+				// final Intent intent = new Intent(Intent.ACTION_MAIN, null);
+				// intent.addCategory(Intent.CATEGORY_LAUNCHER);
+				// final ComponentName cn = new ComponentName(
+				// "com.android.settings",
+				// "com.android.settings.wifi.WifiSettings");
+				// intent.setComponent(cn);
+				// intent.setFlags(intent.FLAG_ACTIVITY_NEW_TASK);
+				// startActivity(intent);
+
+				setWifi(true);
+
 				AndroidUDP.this.finish();
 			}
 		});
-		
-		Toast.makeText(this, "Author: tomxue@outlook.com", Toast.LENGTH_LONG).show();
+
+		Toast.makeText(this, "Author: tomxue@outlook.com", Toast.LENGTH_LONG)
+				.show();
 	}
-	
+
 	private void scan() {
 		wifiApManager.getClientList(false, new FinishScanListener() {
 
 			@Override
 			public void onFinishScan(final ArrayList<ClientScanResult> clients) {
 
-				recvText.setText("WifiApState: " + wifiApManager.getWifiApState() + "\n\n");
+				recvText.setText("WifiApState: "
+						+ wifiApManager.getWifiApState() + "\n\n");
 				recvText.append("Clients: \n");
 				for (ClientScanResult clientScanResult : clients) {
 					recvText.append("####################\n");
-					recvText.append("IpAddr: " + clientScanResult.getIpAddr() + "\n");
-					recvText.append("Device: " + clientScanResult.getDevice() + "\n");
-					recvText.append("HWAddr: " + clientScanResult.getHWAddr() + "\n");
-					recvText.append("isReachable: " + clientScanResult.isReachable() + "\n");
+					recvText.append("IpAddr: " + clientScanResult.getIpAddr()
+							+ "\n");
+					recvText.append("Device: " + clientScanResult.getDevice()
+							+ "\n");
+					recvText.append("HWAddr: " + clientScanResult.getHWAddr()
+							+ "\n");
+					recvText.append("isReachable: "
+							+ clientScanResult.isReachable() + "\n");
 				}
 			}
 		});
 	}
-	
+
 	public boolean onCreateOptionsMenu(Menu menu) {
 		menu.add(0, 0, 0, "Get Clients");
 		menu.add(0, 1, 0, "Open AP");
@@ -238,7 +252,17 @@ public class AndroidUDP extends Activity {
 	protected void onStart() {
 		super.onStart();
 		RecvThread recvThread = new RecvThread();
-		new Thread(recvThread).start();		
+		new Thread(recvThread).start();
+		
+		TimerTask task = new TimerTask(){
+		      public void run() {  
+		       setWifi(false);
+		   }  
+		};
+		
+		Timer timer = new Timer(true);
+		timer.schedule(task,1000); // 延时1000ms后执行
+		//timer.cancel(); // 退出计时器
 	}
 
 	public class RecvThread implements Runnable {
@@ -258,6 +282,38 @@ public class AndroidUDP extends Activity {
 				// } catch (InterruptedException e) {
 				// e.printStackTrace();
 				// }
+			}
+		}
+	}
+
+	/**
+	 * 是否开启 wifi true：开启; false：关闭
+	 * 
+	 * 一定要加入权限： <uses-permission
+	 * android:name="android.permission.ACCESS_WIFI_STATE"></uses-permission>
+	 * <uses-permission
+	 * android:name="android.permission.CHANGE_WIFI_STATE"></uses-permission>
+	 * 
+	 * 
+	 * @param isEnable
+	 */
+	public void setWifi(boolean isEnable) {
+
+		//
+		if (mWifiManager == null) {
+			mWifiManager = (WifiManager) this.getSystemService(AndroidUDP.WIFI_SERVICE);
+		}
+
+		System.out.println("wifi====" + mWifiManager.isWifiEnabled());
+		if (isEnable) {
+			// 开启wifi
+			if (!mWifiManager.isWifiEnabled()) {
+				mWifiManager.setWifiEnabled(true);
+			}
+		} else {
+			// 关闭 wifi
+			if (mWifiManager.isWifiEnabled()) {
+				mWifiManager.setWifiEnabled(false);
 			}
 		}
 	}
